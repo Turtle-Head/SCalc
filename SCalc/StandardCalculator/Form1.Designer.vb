27963c308be418b2579ec25df5a52b44b4d9918c
James Fehr <Turtle-Head@users.noreply.github.com>
2023-02-28 13:12:20 -0800

    Initiating Repository
    The completed exercise to Create a Standard Calculator

---
+++ SCalc/StandardCalculator/Form1.Designer.vb
@@ -0,0 +1,272 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Clr = New Button()
+        bksp = New Button()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        Button16 = New Button()
+        resultbox = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Clr
+        ' 
+        Clr.BackColor = Color.Red
+        Clr.Location = New Point(250, 41)
+        Clr.Name = "Clr"
+        Clr.Size = New Size(75, 43)
+        Clr.TabIndex = 1
+        Clr.Text = "C"
+        Clr.UseVisualStyleBackColor = False
+        ' 
+        ' bksp
+        ' 
+        bksp.BackColor = Color.Red
+        bksp.Location = New Point(169, 41)
+        bksp.Name = "bksp"
+        bksp.Size = New Size(75, 47)
+        bksp.TabIndex = 2
+        bksp.Text = "<<"
+        bksp.UseVisualStyleBackColor = False
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(250, 95)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 39)
+        Button1.TabIndex = 3
+        Button1.Text = "9"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(169, 95)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 39)
+        Button2.TabIndex = 4
+        Button2.Text = "8"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(88, 95)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 39)
+        Button3.TabIndex = 5
+        Button3.Text = "7"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(250, 140)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(75, 36)
+        Button4.TabIndex = 6
+        Button4.Text = "6"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(169, 140)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(75, 36)
+        Button5.TabIndex = 7
+        Button5.Text = "5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(88, 140)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(75, 36)
+        Button6.TabIndex = 8
+        Button6.Text = "4"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(250, 182)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(75, 35)
+        Button7.TabIndex = 9
+        Button7.Text = "3"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(169, 182)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(75, 35)
+        Button8.TabIndex = 10
+        Button8.Text = "2"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(88, 182)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(75, 35)
+        Button9.TabIndex = 11
+        Button9.Text = "1"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.BackColor = Color.LightCyan
+        Button10.Location = New Point(88, 223)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(75, 47)
+        Button10.TabIndex = 12
+        Button10.Text = "."
+        Button10.UseVisualStyleBackColor = False
+        ' 
+        ' Button11
+        ' 
+        Button11.Location = New Point(169, 223)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(75, 47)
+        Button11.TabIndex = 13
+        Button11.Text = "0"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Button12
+        ' 
+        Button12.BackColor = Color.LightSkyBlue
+        Button12.Location = New Point(7, 223)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(75, 47)
+        Button12.TabIndex = 14
+        Button12.Text = "/"
+        Button12.UseVisualStyleBackColor = False
+        ' 
+        ' Button13
+        ' 
+        Button13.BackColor = Color.LightSkyBlue
+        Button13.Location = New Point(7, 182)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(75, 35)
+        Button13.TabIndex = 15
+        Button13.Text = "*"
+        Button13.UseVisualStyleBackColor = False
+        ' 
+        ' Button14
+        ' 
+        Button14.BackColor = Color.LightSkyBlue
+        Button14.Location = New Point(7, 140)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(75, 36)
+        Button14.TabIndex = 16
+        Button14.Text = "-"
+        Button14.UseVisualStyleBackColor = False
+        ' 
+        ' Button15
+        ' 
+        Button15.BackColor = Color.LightSkyBlue
+        Button15.Location = New Point(7, 95)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(75, 39)
+        Button15.TabIndex = 17
+        Button15.Text = "+"
+        Button15.UseVisualStyleBackColor = False
+        ' 
+        ' Button16
+        ' 
+        Button16.BackColor = Color.SpringGreen
+        Button16.Location = New Point(250, 223)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(75, 47)
+        Button16.TabIndex = 18
+        Button16.Text = "="
+        Button16.UseVisualStyleBackColor = False
+        ' 
+        ' resultbox
+        ' 
+        resultbox.Location = New Point(12, 12)
+        resultbox.Name = "resultbox"
+        resultbox.Size = New Size(318, 23)
+        resultbox.TabIndex = 19
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Silver
+        ClientSize = New Size(337, 278)
+        Controls.Add(resultbox)
+        Controls.Add(Button16)
+        Controls.Add(Button15)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(bksp)
+        Controls.Add(Clr)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Clr As Button
+    Friend WithEvents bksp As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents resultbox As TextBox
+End Class
---
+++ SCalc/StandardCalculator/Form1.vb
@@ -0,0 +1,97 @@
+﻿Public Class Form1
+    Dim calcs As Integer = 0
+    Dim operat As String = ""
+
+    Private Sub Clr_Click(sender As Object, e As EventArgs) Handles Clr.Click
+        resultbox.Clear()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        resultbox.Text += "9"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        resultbox.Text += "8"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        resultbox.Text += "7"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        resultbox.Text += "6"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        resultbox.Text += "5"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        resultbox.Text += "4"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        resultbox.Text += "3"
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        resultbox.Text += "2"
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        resultbox.Text += "1"
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        resultbox.Text += "0"
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        resultbox.Text += "."
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        calcs = resultbox.Text
+        resultbox.Clear()
+        operat = "+"
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        calcs = resultbox.Text
+        resultbox.Clear()
+        operat = "-"
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        calcs = resultbox.Text
+        resultbox.Clear()
+        operat = "*"
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        calcs = resultbox.Text
+        resultbox.Clear()
+        operat = "/"
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        If (operat = "+") Then
+            resultbox.Text += calcs
+        ElseIf (operat = "-") Then
+            resultbox.Text -= calcs
+        ElseIf (operat = "*") Then
+            resultbox.Text *= calcs
+        ElseIf (operat = "/") Then
+            resultbox.Text /= calcs
+        End If
+        calcs = 0
+        operat = ""
+    End Sub
+
+    Private Sub bksp_Click(sender As Object, e As EventArgs) Handles bksp.Click
+        Dim ilenght As Integer = resultbox.TextLength - 1
+        Dim result As String = resultbox.Text
+        result = result.Substring(0, ilenght)
+        resultbox.Text = result
+    End Sub
+End Class
